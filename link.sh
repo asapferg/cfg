b4f67a5023ded690f7f01a2781d71d82328a17e0
chrobles <chrobles4@gmail.com>
2020-05-26 11:28:11 -0400

    add ssh-config, update filenames, update link.sh

--- link.sh
+++ link.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
-rm $HOME/.bashrc
-rm $HOME/.bash_aliases
-rm $HOME/.profile
+rm -f $HOME/.bashrc
+rm -f $HOME/.bash_aliases
+rm -f $HOME/.profile
+rm -f $HOME/.ssh/config
 
-ln -s $(pwd)/.bashrc       $HOME/.bashrc
-ln -s $(pwd)/.bash_aliases $HOME/.bash_aliases
-ln -s $(pwd)/.profile      $HOME/.profile
+ln -s $(pwd)/bashrc       $HOME/.bashrc
+ln -s $(pwd)/bash_aliases $HOME/.bash_aliases
+ln -s $(pwd)/profile      $HOME/.profile
+ln -s $(pwd)/ssh-config    $HOME/.ssh/config
